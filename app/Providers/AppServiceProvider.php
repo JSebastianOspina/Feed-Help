ab4046dce60d740fcacc02eb71f1652707efa828
Sebastian Ospina <sebastianospina@Mac-mini-de-Sebastian.local>
2022-01-02 18:39:42 -0500

    Fix schema too long bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//
+        Schema::defaultStringLength(191);
+
     }
 }
